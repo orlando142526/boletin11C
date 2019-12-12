4e12ea12099d9b162a53be3d9d4cb945625cc8c8
orlando142526 <orlando142526@gmail.com>
2019-12-12 23:27:39 +0100

    first commit

---
+++ boletin11c/Boletin11C.java
@@ -0,0 +1,18 @@
+
+package boletin11c;
+
+
+public class Boletin11C {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Date a=new Date();
+        a.setAny(2020);
+        a.setMes(12);
+        a.setDia(31);
+        System.out.println("Dia "+a.getDia()+"\nMes "+a.getMes()+"\nAño "+a.getAny());
+    }
+    
+}
+    
---
+++ boletin11c/Date.java
@@ -0,0 +1,58 @@
+package boletin11c;
+
+
+public class Date {
+    private int dia;
+    private int mes;
+    private int any;
+
+    public Date(int d, int m, int a) {
+        dia = d;
+        mes = m;
+        any = a;
+    }
+
+    public Date() {
+        dia = 0;
+        mes = 0;
+        any = 0000;
+    }
+    
+    public int getDia() {
+        return dia;
+    }
+
+   public void setDia(int d) {
+        if (mirarFecha(d) == true) {
+            dia = d;
+        }
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int m) {
+        if (m < 13) {
+            mes = m;
+        }
+    }
+
+    public int getAny() {
+        return any;
+    }
+
+    public void setAny(int a) {
+        any = a;
+    }
+     public boolean mirarFecha(int dia) {
+        if (((dia < 32 && dia > 0) && (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12))
+                || ((dia < 31 && dia > 0) && (mes == 4 || mes == 6 || mes == 9 || mes == 11))
+                || (((dia < 29 && dia > 0) && (mes == 2)))) {
+            return true;
+        } else {
+            System.out.println("La fecha es incorrecta");
+            return false;
+        }
+     }
+}
